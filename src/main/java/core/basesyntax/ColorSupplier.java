7b96465cfdb407d457a736e9cf63ef1856245b88
Sniklz <tuturox91@gmail.com>
2025-06-19 15:21:06 +0200

    Finish task

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,14 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
+
+    private final Random rand = new Random();
+    private final Colors[] colors = Colors.values();
+
+
     public String getRandomColor() {
-        return null;
+        return colors[rand.nextInt(colors.length)].name();
     }
 }
---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,16 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+
+    private static final int RAND_LIMIT = 101;
+    private final Random rand = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+
+
+    public Ball getRandomBall() {
+
+        return new Ball(colorSupplier.getRandomColor(), (rand.nextInt(RAND_LIMIT)));
+    };
+}
