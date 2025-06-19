3f24dc8a9a39fbe23d28724ed2f8309ef9a2930c
Sniklz <tuturox91@gmail.com>
2025-06-19 14:49:17 +0200

    Make master clear

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,19 +1,7 @@
 package core.basesyntax;
 
-import java.util.Scanner;
-
 public class Application {
     public static void main(String[] args) {
-
-        Ball[] balls = {
-                Lottery.getRandomBall(),
-                Lottery.getRandomBall(),
-                Lottery.getRandomBall(),
-        };
-
-        for (Ball ball: balls)
-        {
-            System.out.println(ball.toString());
-        }
+        // create three balls using class Lottery and print information about them in console
     }
 }
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,14 +1,7 @@
 package core.basesyntax;
 
-import java.util.Random;
-
 public class ColorSupplier {
     public String getRandomColor() {
-
-        Random random = new Random(); //random obj
-
-        MyColors[] allColors = MyColors.values(); //all colors form enum to array
-
-        return allColors[random.nextInt(allColors.length)].toString(); //random int and get color from array then convert toString
+        return null;
     }
 }
